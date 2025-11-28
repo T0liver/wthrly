66c839ce7f1dc115e27d86da9a3db1837d6e652c
T0liver <taschnero@edu.bme.hu>
2025-11-28 21:32:24 +0100

    Add openweathermap to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ function Footer() {
     return <div className="footer-container">
         <div className="footer-text">T0liver © 2025</div>
         <div className="footer-text">Icons by <a href="http://icons8.com" target="_blank" rel="noopener noreferrer">Icons8</a>.</div>
+        <div className="footer-text">Getting data from <a href="http://openweathermap.org/api/" target="_blank" rel="noopener noreferrer">OpenWeatherMap</a>.</div>
     </div>;
 }
 
