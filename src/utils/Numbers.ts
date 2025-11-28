5462ad6206b4500f1b853085b28f170f43a512e7
T0liver <taschnero@edu.bme.hu>
2025-11-28 12:07:44 +0100

    Add some non-used temperature units Just for fun shhh

--- src/utils/Numbers.ts
+++ src/utils/Numbers.ts
@@ -16,3 +16,21 @@ function gradToRad(grad: number) {
 }
 
 export { gradToRad };
+
+function celciusToFahrenheit(celsius: number) {
+	return (celsius * 1.8) + 32;
+}
+
+export { celciusToFahrenheit };
+
+function celsiusToKelvin(celsius: number) {
+	return celsius + 273.15;
+}
+
+export { celsiusToKelvin };
+
+function celsiusToReaumur(celsius: number) {
+	return celsius / 1.25;
+}
+
+export { celsiusToReaumur };
