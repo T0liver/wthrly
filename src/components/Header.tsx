aa1f1979728ebbd84bc3b5734caa3b8905672310
T0liver <taschnero@edu.bme.hu>
2025-11-28 16:05:01 +0100

    Add logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import "../assets/header.css";
+import Icon from "./Icon";
 
 function Header({ text = "Wthrly" }: { text?: string }) {
     return (
         <header className="app-header">
             <h1 className="app-title">{text}</h1>
+            < Icon name="clouds-and-sun" size={150} />
         </header>
     );
 }
