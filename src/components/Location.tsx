ee6edb774ce09b91f418778b42171820c2fd181a
T0liver <taschnero@edu.bme.hu>
2025-11-28 13:22:45 +0100

    Add handle events to locaiton bar

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,11 +1,38 @@
+import { useState, useRef } from "react";
 import "../assets/location.css";
 import Icon from "./Icon";
 
-function Location({ name }: { name?: string }) {
+function Location({ name: initialName }: { name?: string }) {
+	const [name, setName] = useState(initialName);
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setName(event.currentTarget.value);
+	};
+
+	const handleSearch = () => {
+		// Later will call something
+	};
+
+	const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === "Enter") {
+			handleSearch();
+			inputRef.current?.blur();
+		}
+	};
+
 	return (
 		<div className="location-container">
 			<Icon name="air" size={30} />
-			<span className="location-name">{name}</span>
+			<input
+				ref={inputRef}
+				type="text"
+				className="location-name"
+				value={name}
+				onChange={handleInputChange}
+				onKeyDown={handleKeyDown}
+				placeholder="Search for a city"
+			/>
 		</div>
 	);
 }
