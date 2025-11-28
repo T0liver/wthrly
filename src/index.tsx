1a825066353d5fefb86ac05c0263364ecffba4e4
T0liver <taschnero@edu.bme.hu>
2025-11-28 23:15:05 +0100

    Add city name dinamically

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,46 @@
 import "./assets/main.css";
 import { render } from "preact";
-import { useState, useEffect } from "preact/hooks";
 import Location from "./components/Location";
 import Header from "./components/Header";
 import Temperature from "./components/Temperature";
 import AirConditions from "./components/AirConditions";
 import Footer from "./components/Footer";
 import DaysForecast from "./components/DaysForecast";
-import { getWeatherData, UIData } from "./services/weatherService";
 import Loading from "./components/Loading";
+import { WeatherProvider, useWeather } from "./context/WeatherContext";
+
+function WeatherApp() {
+	const { weatherData, isLoading } = useWeather();
+
+	if (isLoading || !weatherData) {
+		return <Loading />;
+	}
+
+	return (
+		<div className="app-container">
+			<Header />
+			<Location name={weatherData.name} />
+			<Temperature value={weatherData.temp} />
+			<DaysForecast />
+			<div className="air-conditions-wrapper">
+				<AirConditions
+					realFeel={weatherData.feels_like}
+					wind={weatherData.wind_speed}
+					clouds={weatherData.clouds_all}
+					humidity={weatherData.humidity}
+				/>
+			</div>
+			<Footer />
+		</div>
+	);
+}
 
 function App() {
-    const location = "Budapest";
-    const [currentWeather, setCurrentWeather] = useState<UIData | null>(null);
-
-    useEffect(() => {
-        getWeatherData(location).then(setCurrentWeather);
-    }, [location]);
-
-    if (!currentWeather) {
-        return <Loading />;
-    }
-
-    return <div className="app-container">
-            <Header />
-            <Location name={location} />
-            <Temperature value={currentWeather.temp} />
-            <DaysForecast />
-            <div className="air-conditions-wrapper">
-                <AirConditions
-                    realFeel={currentWeather.feels_like}
-                    wind={currentWeather.wind_speed}
-                    clouds={currentWeather.clouds_all}
-                    humidity={currentWeather.humidity}
-                />
-            </div>
-            <Footer />
-        </div>;
+	return (
+		<WeatherProvider>
+			<WeatherApp />
+		</WeatherProvider>
+	);
 }
 
 render(<App />, document.getElementById('app'));
