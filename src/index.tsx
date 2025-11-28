8f669c5b63225cecd3e370c8a9070941120809c1
T0liver <taschnero@edu.bme.hu>
2025-11-28 21:26:00 +0100

    Integrate the current weather on the site

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,38 @@
 import "./assets/main.css";
 import { render } from "preact";
+import { useState, useEffect } from "preact/hooks";
 import Location from "./components/Location";
 import Header from "./components/Header";
 import Temperature from "./components/Temperature";
 import AirConditions from "./components/AirConditions";
 import Footer from "./components/Footer";
 import DaysForecast from "./components/DaysForecast";
+import { getWeatherData, UIData } from "./services/weatherService";
 
 function App() {
+    const location = "Budapest";
+    const [currentWeather, setCurrentWeather] = useState<UIData | null>(null);
+
+    useEffect(() => {
+        getWeatherData(location).then(setCurrentWeather);
+    }, [location]);
+
+    if (!currentWeather) {
+        return <div>Loading...</div>;
+    }
+
     return <div className="app-container">
             <Header />
-            <Location name="Budapest" />
-            <Temperature value={22} />
+            <Location name={location} />
+            <Temperature value={currentWeather.temp} />
             <DaysForecast />
             <div className="air-conditions-wrapper">
-                <AirConditions realFeel={20} wind={5} clouds={75} humidity={60} />
+                <AirConditions
+                    realFeel={currentWeather.feels_like}
+                    wind={currentWeather.wind_speed}
+                    clouds={currentWeather.clouds_all}
+                    humidity={currentWeather.humidity}
+                />
             </div>
             <Footer />
         </div>;
