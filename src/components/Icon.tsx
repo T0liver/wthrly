326702ebe3396964b2370fd571624d753c823cb2
T0liver <taschnero@edu.bme.hu>
2025-11-28 15:32:01 +0100

    Update icons names

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,7 +1,7 @@
 function Icon({ name = "cloud", size = 100 }: { name?: string; size?: number }) {
 	return (
 		<img
-			src={`/icons/icons8-${name}-100.png`}
+			src={`/icons/icons8-${name}.png`}
 			alt="weather icon"
 			width={size}
 			height={size}
