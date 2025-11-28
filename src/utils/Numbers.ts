b062446d86cccb8c61fe2f68017d0b95b6dd9960
T0liver <taschnero@edu.bme.hu>
2025-11-28 16:12:07 +0100

    Add fraction digits to the gradToX functions

--- src/utils/Numbers.ts
+++ src/utils/Numbers.ts
@@ -11,26 +11,26 @@ function formatNumber(
 
 export { formatNumber };
 
-function gradToRad(grad: number) {
-    return grad * (Math.PI / 180);
+function gradToRad(grad: number, fractionDigits = 8) {
+    return Number((grad * (Math.PI / 180)).toFixed(fractionDigits));
 }
 
 export { gradToRad };
 
-function celciusToFahrenheit(celsius: number) {
-	return (celsius * 1.8) + 32;
+function celciusToFahrenheit(celsius: number, fractionDigits = 3) {
+	return Number(((celsius * 1.8) + 32).toFixed(fractionDigits));
 }
 
 export { celciusToFahrenheit };
 
-function celsiusToKelvin(celsius: number) {
-	return celsius + 273.15;
+function celsiusToKelvin(celsius: number, fractionDigits = 2) {
+	return Number((celsius + 273.15).toFixed(fractionDigits));
 }
 
 export { celsiusToKelvin };
 
-function celsiusToReaumur(celsius: number) {
-	return celsius / 1.25;
+function celsiusToReaumur(celsius: number, fractionDigits = 3) {
+	return Number((celsius / 1.25).toFixed(fractionDigits));
 }
 
 export { celsiusToReaumur };
