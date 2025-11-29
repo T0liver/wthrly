c35bfe18024fcb26617555136f496cd9c1358f47
T0liver <taschnero@edu.bme.hu>
2025-11-29 10:55:23 +0100

    Rename key env variable name

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,7 +3,7 @@ import ky from 'ky';
 const apiClient = ky.create({
   prefixUrl: 'https://api.openweathermap.org/data/2.5',
   searchParams: {
-    appid: import.meta.env.VITE_APP_API_KEY,
+    appid: import.meta.env.VITE_OPENWEATHER_KEY,
   },
 });
 
