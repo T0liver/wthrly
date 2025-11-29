e527195ab58a84c53ec0ca5428d4fd08b610b397
T0liver <taschnero@edu.bme.hu>
2025-11-29 12:17:35 +0100

    Update weather icons LUT

--- src/utils/weatherIcon.ts
+++ src/utils/weatherIcon.ts
@@ -1,22 +1,70 @@
 const iconMap: { [key: string]: string } = {
-    "01d": "sun",
-    "01n": "night",
-    "02d": "partly-cloudy-day",
-    "02n": "clouds",
-    "03d": "cloud",
-    "03n": "cloud",
-    "04d": "clouds",
-    "04n": "clouds",
-    "09d": "heavy-rain",
-    "09n": "heavy-rain",
-    "10d": "light-rain",
-    "10n": "light-rain",
-    "11d": "stormy-weather",
-    "11n": "stormy-weather",
-    "13d": "light-snow",
-    "13n": "light-snow",
-    "50d": "haze",
-    "50n": "haze",
+    // Clear / fair
+    "clearsky": "sun",
+    "clearsky_day": "sun",
+    "clearsky_night": "night",
+    "fair": "partly-cloudy-day",
+    "fair_day": "partly-cloudy-day",
+    "fair_night": "clouds",
+
+    // Cloud coverage
+    "partlycloudy": "partly-cloudy-day",
+    "partlycloudy_day": "partly-cloudy-day",
+    "partlycloudy_night": "clouds",
+    "cloudy": "clouds",
+    "cloud": "cloud",
+
+    // Fog / haze
+    "fog": "fog",
+    "haze": "haze",
+
+    // Rain
+    "lightrain": "light-rain",
+    "lightrainandthunder": "cloud-lightning",
+    "lightrainshowers": "light-rain",
+    "lightrainshowersandthunder": "cloud-lightning",
+
+    "rain": "rain",
+    "rainandthunder": "stormy-weather",
+    "rainshowers": "rain-cloud",
+    "rainshowersandthunder": "cloud-lightning",
+    "heavyrain": "heavy-rain",
+    "heavyrainandthunder": "storm-with-heavy-rain",
+    "heavyrainshowers": "storm-with-heavy-rain",
+    "heavyrainshowersandthunder": "storm-with-heavy-rain",
+
+    // Sleet / mixed
+    "lightsleet": "sleet",
+    "lightsleetandthunder": "sleet",
+    "lightsleetshowers": "sleet",
+    "lightssleetshowersandthunder": "sleet",
+    "sleet": "sleet",
+    "sleetandthunder": "sleet",
+    "sleetshowers": "sleet",
+    "sleetshowersandthunder": "sleet",
+    "heavysleet": "sleet",
+    "heavysleetandthunder": "sleet",
+    "heavysleetshowers": "sleet",
+    "heavysleetshowersandthunder": "sleet",
+
+    // Snow
+    "lightsnow": "light-snow",
+    "lightsnowandthunder": "light-snow",
+    "lightsnowshowers": "light-snow",
+    "lightssnowshowersandthunder": "light-snow",
+    "snow": "snow-storm",
+    "snowandthunder": "snow-storm",
+    "snowshowers": "snow-storm",
+    "snowshowersandthunder": "snow-storm",
+    "heavysnow": "snow-storm",
+    "heavysnowandthunder": "snow-storm",
+    "heavysnowshowers": "snow-storm",
+    "heavysnowshowersandthunder": "snow-storm",
+
+    // Wind / other
+    "wind": "wind",
+    "tornado": "tornado",
+    "icy": "icy",
 };
 
 export const getWeatherIcon = (iconCode: string) => {
