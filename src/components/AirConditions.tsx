b8a5cc886d4c00d2b8d21917c825b7eeb9ee77e4
T0liver <taschnero@edu.bme.hu>
2025-11-29 22:43:07 +0100

    Rename air conditions corresponding to the site's manifesto
    Manifesto is like: don't let the robots to read it easily

--- src/components/AirConditions.tsx
+++ src/components/AirConditions.tsx
@@ -3,7 +3,7 @@ import AirConditionElement from "./AirConditionElement";
 
 function AirConditions({ realFeel, wind, clouds, humidity }: { realFeel?: number, wind?: number, clouds?: number, humidity?: number }) {
     return <div className="air-conditions-container">
-        <div className="air-condition-header">Air Conditions</div>
+        <div className="air-condition-header">Ar Cndtns</div>
         <AirConditionElement label="Rl Fl" value={realFeel} unit="˚C" iconName="thermometer" />
         <AirConditionElement label="Wnd" value={wind} unit="m/s" iconName="wind" />
         <AirConditionElement label="Clds" value={clouds} unit="%" iconName="cloud" />
