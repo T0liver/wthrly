693045409a00309952d60e8597d4a415bf109a49
T0liver <taschnero@edu.bme.hu>
2025-11-29 12:56:31 +0100

    Add more footer copyrigth

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ function Footer() {
     return <div className="footer-container">
         <div className="footer-text">T0liver © 2025</div>
         <div className="footer-text">Icons by <a href="http://icons8.com" target="_blank" rel="noopener noreferrer">Icons8</a>.</div>
-        <div className="footer-text">Getting data from <a href="http://openweathermap.org/api/" target="_blank" rel="noopener noreferrer">OpenWeatherMap</a>.</div>
+        <div className="footer-text">Getting weather data from<br/><a href="https://api.met.no/" target="_blank" rel="noopener noreferrer">The Norwegian Meteorological Institute</a>.</div>
+        <div className="footer-text">Getting geo location from <a href="https://nominatim.openstreetmap.org/" target="_blank" rel="noopener noreferrer">OpenStreetMap's Nominatim</a>.</div>
+        <div className="footer-text">The Celsius radian idea is from Jávor Márton.</div>
     </div>;
 }
 
