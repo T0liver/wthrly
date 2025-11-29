1cda62a75a3e5628c9bd4d544e9757f9c4fdb4a5
T0liver <taschnero@edu.bme.hu>
2025-11-30 00:43:24 +0100

    Typo

--- src/components/SideCard.tsx
+++ src/components/SideCard.tsx
@@ -6,11 +6,11 @@ function SideCard() {
             <p>Did you find a bug?</p>
             <p>Write me a github issue!</p>
             <a
-                href="https://github.com/T0liver/wtrly/issues"
+                href="https://github.com/T0liver/wthrly/issues"
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                github.com/T0liver/wtrly/issues
+                github.com/T0liver/wthrly/issues
             </a>
         </div>
     );
